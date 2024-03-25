3ee428d947e7a88469c1cb3605d37651ecfabe61
Andrii Sukhenko <andreysuha2@gmail.com>
2024-03-25 18:02:57 +0200

    add heltchecker

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI, APIRouter
+from fastapi import FastAPI, APIRouter, HTTPException
+from sqlalchemy import text
 from app.settings import settings
+from app.db import DBConnectionDep
 from datetime import datetime, UTC
 from users.routers import session_router, user_router
 from photos.routers import photos_router
@@ -13,8 +15,14 @@ base_router = APIRouter(tags=['base'])
 routers = [base_router, session_router, user_router, photos_router, comments_router]
 
 @base_router.get("/")
-def status():
-    return {"version": settings.app.VERSION, "name": settings.app.NAME, "status": "ok", "env": settings.app.ENV, "datetime": datetime.now(UTC)}
+def status(db: DBConnectionDep):
+    try:
+        result = db.execute(text("SELECT 1"))
+        print(result)
+        return {"version": settings.app.VERSION, "name": settings.app.NAME, "status": "ok", "env": settings.app.ENV, "datetime": datetime.now(UTC)}
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=500, detail="Error db connection")
 
 [app.include_router(router, prefix=settings.app.BASE_URL_PREFIX) for router in routers]
 
